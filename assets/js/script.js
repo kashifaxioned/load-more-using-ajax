3df79be4cb0535117855f8b94e586897f5dd5a52
kashifaxioned <kashifa@axioned.com>
2022-04-15 02:00:38 +0530

    JS functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,47 @@
 
 */
 
+// initializing elements
+
+let contentItem = $(".content-list")
+let listData = []
+$(".load-btn").click(showList)
+
+// calling function for load data
+showList()
+
+// declaring function for load data
+function showList() {
+
+  let listCount;
+  let showItem = $(".content-item")
+  let numOfItemsToShow = 5
+
+  // selecting how many elements should be selected
+  if (showItem.length !== 0) listCount = showItem.length
+  else listCount = 0
 
 
+  let xhr = new XMLHttpRequest()
+  xhr.open('GET', "https://jsonplaceholder.typicode.com/photos", true)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      listData = JSON.parse(xhr.responseText)
+      for (let i = listCount; i < listCount + numOfItemsToShow; i++) {
+        const x = listData[i];
+        if (x.id < listCount) {
+          break;
+        } else {
+          contentItem.append(`
+        <li class="content-item">
+        <h2>${x.id}</h2>
+        <img src=${x.url} alt=${x.id}/>
+         </li>
+        `)
+        }
+      }
+    }
+  }
+  xhr.send()
+}
